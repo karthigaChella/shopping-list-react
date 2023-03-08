5c8a1797012b8ea696855688a28c5a4a0e7dc8a3
Karthiga <karthigachellamuthu@yahoo.com>
2023-03-08 19:47:35 +0000

    dynamic list rendering

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -13,10 +13,11 @@ function Expenses(props) {
         <div>
             <ExpenseFilter onChangeExpenseYearHandler={changeExpenseYearHandler}/>
         <Card className='expenses'>
-            <ExpenseItem  date={props.expenses[0].date} title={props.expenses[0].title} amount={props.expenses[0].amount}></ExpenseItem>
+            {props.expenses.map(expense => <ExpenseItem date={expense.date} title={expense.title} amount={expense.amount}/>)}
+            {/* <ExpenseItem  date={props.expenses[0].date} title={props.expenses[0].title} amount={props.expenses[0].amount}></ExpenseItem>
             <ExpenseItem  date={props.expenses[1].date} title={props.expenses[1].title} amount={props.expenses[1].amount}></ExpenseItem>
             <ExpenseItem  date={props.expenses[2].date} title={props.expenses[2].title} amount={props.expenses[2].amount}></ExpenseItem>
-            <ExpenseItem  date={props.expenses[3].date} title={props.expenses[3].title} amount={props.expenses[3].amount}></ExpenseItem>
+            <ExpenseItem  date={props.expenses[3].date} title={props.expenses[3].title} amount={props.expenses[3].amount}></ExpenseItem> */}
         </Card>
         </div> 
     );
